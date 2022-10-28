f78f27ef0a33c84e0dca602b9f6dce9e9dc57c86
najat <najatt.ismail@gmail.com>
2022-10-28 13:25:30 +0300

    get event by id done

--- components/events/event-details.js
+++ components/events/event-details.js
@@ -1,7 +1,8 @@
 import classes from '../events/event-details.module.css';
+import btnclass from '../events/event-item.module.css';
 
 export default function EventDetails(props) {
-    const { title, discription, image, date, location } = props;
+    const { title, description, image, date, location } = props;
 
     const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
         day: 'numeric',
@@ -16,14 +17,14 @@ export default function EventDetails(props) {
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{title}</h2>
-            <p class="mb-4">{discription}</p>
+            <p class="mb-4">{description}</p>
             <time>{humanReadableDate}</time>
             <address>{formattedAddress}</address>
         </div>
         <div class="mt-8">
             <img class="w-full rounded-lg" src={'/' + image} alt="event images"/>
         </div>
-        <a className={classes.btn} href={gobacklink}>
+        <a className={btnclass.btn} href={gobacklink}>
             <span>Go Back</span>
             <span className="">
             
--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -38,7 +38,7 @@ export async function getStaticProps(context) {
   // params contains the event `id`.
   // If the route is like /events/1, then params.id is 1
   const eventId = context.params.eventid;
-  const event = await axios.get("http://127.0.0.1:8000/api/events/"+{eventId});
+  const event = await axios.get(`http://127.0.0.1:8000/api/events/${eventId}`);
   return {
     props: {
       // Pass event data to the page via props
@@ -52,9 +52,10 @@ export async function getStaticPaths() {
   // Call an external API endpoint to get events
   const events = await axios.get("http://127.0.0.1:8000/api/events");
   // Get the paths we want to pre-render based on events
-  const pathsWithParams =  Array.isArray(events)&&events.map((event) => ({
-    params: { eventid: event.id },
+  const pathsWithParams =  events.data.map((event) => ({
+    params: { eventid: event.id.toString()},
   }))
+
   // We'll pre-render only these paths at build time.
   return {
     paths: pathsWithParams,
